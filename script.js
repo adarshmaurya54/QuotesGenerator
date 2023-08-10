7b71cc36592fe46649360fb0c6bb77c923fb6830
Adarsh Maurya <adarshmaurya0022@gmail.com>
2023-08-10 12:35:24 +0530

    timmer wise post refresh

--- script.js
+++ script.js
@@ -7,10 +7,16 @@ const quote = document.getElementById("quote");
 const typeName = document.getElementById("type-name");
 let url2 = "https://hindi-quotes.vercel.app/random";
 // Calling api
-fetch(url2)
-.then(response => response.json())
-.then(data => {
-    console.log(data);
-    quote.innerText = data.quote;
-    typeName.innerText = data.type;
-})
+function quoteGenerate(){
+    fetch(url2)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        quote.innerText = data.quote;
+        typeName.innerText = data.type;
+    })
+}
+setInterval(function(){
+    quoteGenerate();
+},120000);
+quoteGenerate();
