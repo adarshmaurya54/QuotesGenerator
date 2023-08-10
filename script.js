8542999b876a56cd145aebbf1146b73f46b21d3e
Adarsh Maurya <adarshmaurya0022@gmail.com>
2023-08-10 11:34:32 +0530

    This is a quote generator

---
+++ script.js
@@ -0,0 +1,16 @@
+// let apikey = "8Liq3QqC97Bnczyi4mI5aQ==xRCEKAnKU3AKyKn9";
+// let url = "https://api.api-ninjas.com/v1/quotes";
+// const headers = new Headers();
+// headers.append('X-Api-Key', apikey);
+// headers.append('Content-Type', 'application/json');
+const quote = document.getElementById("quote");
+const typeName = document.getElementById("type-name");
+let url2 = "https://hindi-quotes.vercel.app/random";
+// Calling api
+fetch(url2)
+.then(response => response.json())
+.then(data => {
+    console.log(data);
+    quote.innerText = data.quote;
+    typeName.innerText = data.type;
+})
